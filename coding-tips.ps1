a4473370f1ab8bb919b624496cea7f2901aeb5cb
aarvens <aaron@webaaron.com>
2021-12-18 16:32:15 -0800

    first commit

---
+++ coding-tips.ps1
@@ -0,0 +1,9 @@
+
+# here are some quick snippets that will help when you're writing powershell code.
+# Written by Aaron Stevens
+# Adapted from everywhere
+
+# View parameters for a function / command:
+Get-Command -Name <command> -Syntax
+
+# write-verbose as much as possible for logging
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Powershell Toolshed
+In an effort to stop retyping the same thing everyday, this is a collection of powershell templates.
